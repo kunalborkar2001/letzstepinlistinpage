ae85fafc853dc5b278e786e6af5d982b88a7bb8b
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-13 17:40:13 +0530

    added url images to banquet

--- src/Data/Banquet.js
+++ src/Data/Banquet.js
@@ -1,13 +1,13 @@
-import banquet1 from '../Assets/Banquet/banquet1.jpeg'
-import banquet2 from '../Assets/Banquet/banquet2.jpeg'
-import banquet3 from '../Assets/Banquet/banquet3.jpeg'
-import banquet4 from '../Assets/Banquet/banquet4.jpeg'
-import banquet5 from '../Assets/Banquet/banquet5.jpeg'
-import banquet6 from '../Assets/Banquet/banquet6.jpeg'
-import banquet7 from '../Assets/Banquet/banquet7.jpeg'
-import banquet8 from '../Assets/Banquet/banquet8.jpeg'
-import banquet9 from '../Assets/Banquet/banquet9.jpeg'
-import banquet10 from '../Assets/Banquet/banquet10.jpeg'
+let  banquet1 = "https://images.unsplash.com/photo-1572319663329-ac47c4efdef0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D"
+let  banquet2 = 'https://images.unsplash.com/photo-1561593367-66c79c2294e6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhbnF1ZXR8ZW58MHx8MHx8fDA%3D'
+let  banquet3 = 'https://plus.unsplash.com/premium_photo-1661774645265-ce387923cb5b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJhbnF1ZXR8ZW58MHx8MHx8fDA%3D'
+let  banquet4 = 'https://images.unsplash.com/photo-1542665952-14513db15293?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJhbnF1ZXR8ZW58MHx8MHx8fDA%3D'
+let  banquet5 = 'https://images.unsplash.com/photo-1522413452208-996ff3f3e740?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhbnF1ZXR8ZW58MHx8MHx8fDA%3D'
+let  banquet6 = 'https://images.unsplash.com/photo-1484156818044-c040038b0719?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D'
+let  banquet7 = 'https://images.unsplash.com/photo-1624763149686-1893acf73092?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D'
+let  banquet8 = 'https://images.unsplash.com/photo-1524824267900-2fa9cbf7a506?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D'
+let  banquet9 = 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D'
+let  banquet10 = 'https://plus.unsplash.com/premium_photo-1661405720585-d2a451c77724?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmFucXVldHxlbnwwfHwwfHx8MA%3D%3D'
 
 
 
